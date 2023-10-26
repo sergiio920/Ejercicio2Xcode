f3021a265f7e2744ef40bfed891e237d65c518b1
sergiomoron <sergio.moronvidal@cieep.com>
2023-10-26 09:15:01 +0200

    Initial Commit

---
+++ Ejercicio2Xcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejercicio2Xcode
+//
+//  Created by Sergio Morón on 26/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Ejercicio2XcodeTests/Ejercicio2XcodeTests.swift
@@ -0,0 +1,33 @@
+//
+//  Ejercicio2XcodeTests.swift
+//  Ejercicio2XcodeTests
+//
+//  Created by Sergio Morón on 26/10/2023.
+//
+
+import XCTest
+@testable import Ejercicio2Xcode
+
+class Ejercicio2XcodeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
